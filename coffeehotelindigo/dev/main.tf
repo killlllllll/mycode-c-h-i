777b32a0656e699c9e43c9c9373f1fe87c9c2468
killlllllll <104679715+killlllllll@users.noreply.github.com>
2022-07-30 14:33:34 +0530

    Update main.tf

--- coffeehotelindigo/dev/main.tf
+++ coffeehotelindigo/dev/main.tf
@@ -2,18 +2,20 @@ provider "aws" {
  region = "ap-south-1"
 }
 
-module "react-spa-frontend-s3-cf" {
+module "react-spa-frontend-s3cf" {
     source                 = "../../modules/services/react-spa-frontend-s3-cf"
     hosted_zone            = "cloudtoday.click" 
-    domain_name            = "stgft.cloudtoday.click"
+    domain_name            = "test.cloudtoday.click"
     acm_certificate_domain = "cloudtoday.click"
     zonid                  = "Z08978411J71TQQD321FO"
     env                    =  "dev123"
     acm_certificate       = "arn:aws:acm:us-east-1:991008360267:certificate/92f2310b-6c90-4f8c-bf3f-0afc9697c3c4"
-    bucket_name            =  "coffeeindigo-ui"
+    bucket_name            =  "coffeeindigo-bucket"
     acl                    = "private"
-    versioning             = "Disabled"
+    versioning             = "Enabled"
 }
+
+
 
  module "strapi-cms-ecs" {
     source           = "../../modules/services/strapi-cms-ecs/"
